01ca6eb2955e7407771575e6f0f8a2d5859fe132
sansany <ens6636@naver.com>
2023-12-02 20:52:34 +0900

    Dashboard class  lblUser addMouseListener Message

--- src/Hotel/Management/System/Reception.java
+++ src/Hotel/Management/System/Reception.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class Reception extends JFrame {
 
